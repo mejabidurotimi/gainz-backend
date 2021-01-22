144b8d3410bbbe2069d3b6139e28560971086b40
Mejabi Oluwadurotimi <mejabidurotimi@yahoo.com>
2021-01-22 12:59:03 +0100

    Set up signup/signin

--- app.js
+++ app.js
@@ -28,11 +28,16 @@ app.use(bodyParser.json());
 
 // CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "req.headers.origin");
+  // res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With,Content-Type,Accept,Authorization"
   );
+
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Max-Age", "1000000000");
 
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
